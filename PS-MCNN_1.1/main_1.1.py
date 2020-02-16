7262117b27fec809a682c8244c415b3af603d217
许轲 <862453073@qq.com>
2020-02-16 23:08:38 +0800

    调

--- PS-MCNN_1.1/main_1.1.py
+++ PS-MCNN_1.1/main_1.1.py
@@ -414,13 +414,18 @@ def train(train_loader, model, criterion, optimizer, epoch, writer, count,
             #         tp[iii] += 1
             #     elif pred[jjj,iii] == target[jjj,iii] == 0:
             #         tn[iii] += 1
+            a=np.array(target[:,iii])
+            b=np.array(pred[:,iii])
+            ai=set(a)
+            bi=set(b)
+            print('y_true',ai)
+            print('y_pred',bi)
             balance_tmp[iii] = balanced_accuracy_score(target[:,iii].cpu(), pred[:,iii].cpu())
         
         if sum(balance) == 0:
             balance = balance_tmp
         else:
             balance = (torch.Tensor(balance) + torch.Tensor(balance_tmp)) * 0.5
-        print('balance: ', balance)
         mean_balance = torch.mean(balance)
 
         # 每个属性在当前batch的准确率
