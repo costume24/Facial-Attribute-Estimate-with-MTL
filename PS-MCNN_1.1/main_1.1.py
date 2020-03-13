4bf6689f3020aa1066104bb113686ce6c1a277b9
许轲 <862453073@qq.com>
2020-03-13 18:42:19 +0800

    自动命名的优化

--- PS-MCNN_1.1/main_1.1.py
+++ PS-MCNN_1.1/main_1.1.py
@@ -267,7 +267,7 @@ def main():
     args.checkpoint = 'checkpoints_v' + '_'.join([str(args.version), args.set,
                                                  args.focal, args.adaloss,
                                                  str(args.epochs), str(args.train_batch),
-                                                 '{:.0e}'.format(args.lr), str(args.lr_decay)])
+                                                 '{:.0e}'.format(args.lr), str(args.lr_decay), args.xav])
     if not os.path.isdir(args.checkpoint):
         args.checkpoint = mkdir_p(args.checkpoint, 1)
 
