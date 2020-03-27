8a239dd391bd88c701b579d18f4c5fd4da32a2ec
许轲 <862453073@qq.com>
2020-03-27 16:02:59 +0800

    自动命名前缀为arcsin

--- PS-MCNN_1.1/main_1.1.py
+++ PS-MCNN_1.1/main_1.1.py
@@ -296,7 +296,7 @@ def main():
                                  args.lr,
                                  weight_decay=args.weight_decay)
     # optionally resume from a checkpoint
-    args.checkpoint = 'checkpoints_dual_v' + '_'.join([str(args.version), args.set,
+    args.checkpoint = 'checkpoints_arcsin_v' + '_'.join([str(args.version), args.set,
                                                  args.focal, args.adaloss,
                                                  str(args.epochs), str(args.train_batch),
                                                  '{:.0e}'.format(args.lr), str(args.lr_decay), args.prelu])
