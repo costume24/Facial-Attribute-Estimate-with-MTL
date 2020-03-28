77259c27ebbb4ccf8cf5ee9f907737759f8b2125
costume24 <862453073@qq.com>
2020-03-28 21:40:23 +0800

    v100 debug

--- PS-MCNN_1.1/models/psmcnn_v100.py
+++ PS-MCNN_1.1/models/psmcnn_v100.py
@@ -4,37 +4,37 @@ import torch.nn as nn
 import os
 import sys
 
+
 def conv_3x3_bn(inp, oup, stride=1):
-    return nn.Sequential(nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-                         nn.BatchNorm2d(oup), nn.ReLU6(inplace=True))
+    return nn.Sequential(nn.Conv2d(inp, oup, 3, stride, 1, bias=False), nn.BatchNorm2d(oup), nn.ReLU6(inplace=True))
+
 
 def conv_1x1_bn(inp, oup):
-    return nn.Sequential(
-        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-        nn.BatchNorm2d(oup),
-        nn.ReLU6(inplace=True)
-    )
+    return nn.Sequential(nn.Conv2d(inp, oup, 1, 1, 0, bias=False), nn.BatchNorm2d(oup), nn.ReLU6(inplace=True))
+
 
 def conv_3x3_bn_prelu(inp, oup, stride=1):
-    return nn.Sequential(nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-                         nn.BatchNorm2d(oup), nn.PReLU())
+    return nn.Sequential(nn.Conv2d(inp, oup, 3, stride, 1, bias=False), nn.BatchNorm2d(oup), nn.PReLU())
 
 
 def conv_1x1_bn_prelu(inp, oup):
-    return nn.Sequential(nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-                         nn.BatchNorm2d(oup), nn.ReLU6(inplace=True))
+    return nn.Sequential(nn.Conv2d(inp, oup, 1, 1, 0, bias=False), nn.BatchNorm2d(oup), nn.ReLU6(inplace=True))
+
 
 class BasicConv2d(nn.Module):
-
     def __init__(self, in_planes, out_planes, kernel_size, stride, padding=0):
         super(BasicConv2d, self).__init__()
-        self.conv = nn.Conv2d(in_planes, out_planes,
-                              kernel_size=kernel_size, stride=stride,
-                              padding=padding, bias=False) # verify bias false
-        self.bn = nn.BatchNorm2d(out_planes,
-                                 eps=0.001, # value found in tensorflow
-                                 momentum=0.1, # default pytorch value
-                                 affine=True)
+        self.conv = nn.Conv2d(in_planes,
+                              out_planes,
+                              kernel_size=kernel_size,
+                              stride=stride,
+                              padding=padding,
+                              bias=False)  # verify bias false
+        self.bn = nn.BatchNorm2d(
+            out_planes,
+            eps=0.001,  # value found in tensorflow
+            momentum=0.1,  # default pytorch value
+            affine=True)
         self.relu = nn.PReLU()
 
     def forward(self, x):
@@ -43,32 +43,27 @@ class BasicConv2d(nn.Module):
         x = self.relu(x)
         return x
 
-class Block(nn.Module):
 
+class Block(nn.Module):
     def __init__(self, inp, oup, reduction, scale1=1.0, scale2=1.0):
         super(Block, self).__init__()
 
         self.scale1 = scale1
         self.scale2 = scale2
         self.inp = inp
-        self.r = self.inp//reduction
+        self.r = self.inp // reduction
         self.oup = oup
-        self.pool = nn.MaxPool2d(2, 2)
         self.se = SELayer(oup)
         self.branch0 = BasicConv2d(self.inp, self.r, kernel_size=1, stride=1)
 
-        self.branch1 = nn.Sequential(
-            BasicConv2d(self.inp, self.r, kernel_size=1, stride=1),
-            BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1)
-        )
+        self.branch1 = nn.Sequential(BasicConv2d(self.inp, self.r, kernel_size=1, stride=1),
+                                     BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1))
 
-        self.branch2 = nn.Sequential(
-            BasicConv2d(self.inp, self.r, kernel_size=1, stride=1),
-            BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1),
-            BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1)
-        )
+        self.branch2 = nn.Sequential(BasicConv2d(self.inp, self.r, kernel_size=1, stride=1),
+                                     BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1),
+                                     BasicConv2d(self.r, self.r, kernel_size=3, stride=1, padding=1))
 
-        self.conv2d = nn.Conv2d(3*self.r, self.inp, kernel_size=1, stride=1)
+        self.conv2d = nn.Conv2d(3 * self.r, self.inp, kernel_size=1, stride=1)
         self.conv1 = nn.Conv2d(self.inp, self.oup, kernel_size=1, stride=1)
         self.relu = nn.PReLU()
 
@@ -81,18 +76,12 @@ class Block(nn.Module):
         out = out * self.scale1 + x
         out = self.conv1(out)
         out = self.relu(out)
-        out = self.pool(out)
         out = self.se(out) * self.scale2 + out
         return out
 
 
 class psnet(nn.Module):
-
-    def __init__(self,
-                 use_1x1=True,
-                 prelu='no',
-                 reduction=4,
-                 scale=1.0):
+    def __init__(self, use_1x1=True, prelu='no', reduction=4, scale=1.0):
         super().__init__()
         if prelu == 'yes':
             conv3 = conv_3x3_bn_prelu
@@ -102,53 +91,43 @@ class psnet(nn.Module):
             conv1 = conv_1x1_bn
         self.use_1x1 = use_1x1
         self.pool = nn.MaxPool2d(2, 2)
-        self.s_conv = nn.ModuleList([
-            conv3(3, 32),
-            conv3(160, 64),
-            conv3(192, 128),
-            conv3(256, 256),
-            conv3(384, 128)
-        ])  # (5,),s支路的5个卷积层
+        self.s_conv = nn.ModuleList([conv3(3, 32),
+                                     conv3(160, 64),
+                                     conv3(192, 128),
+                                     conv3(256, 256),
+                                     conv3(384, 128)])  # (5,),s支路的5个卷积层
         self.t_conv = nn.ModuleList()
         for _ in range(4):
-            tmp = nn.ModuleList([conv3(3,32),conv3(32,32)])
+            tmp = nn.ModuleList([conv3(3, 32), conv3(32, 32)])
             self.t_conv.append(tmp)
-        self.s_conv_1 = conv3(3,32)
-        self.s_conv_2 = conv3(32,32)
 
         self.t_fc = nn.ModuleList()  # (4,2)，每一行是一个t支路的2个FC层
-        self.s_fc = nn.ModuleList([nn.Linear(3840, 512),
-                                   nn.Linear(512, 512)])  # (2,)，s支路的2个FC层
+        self.s_fc = nn.ModuleList([nn.Linear(3840, 512), nn.Linear(512, 512)])  # (2,)，s支路的2个FC层
         self.output = []  # (4,), 4个支路的输出
-        self.tconv_1x1 = nn.ModuleList() # (4,4)，用1x1卷积进行降维，取代原来的取前32个通道
+        self.tconv_1x1 = nn.ModuleList()  # (4,4)，用1x1卷积进行降维，取代原来的取前32个通道
         for _ in range(4):
-            tmp = nn.ModuleList([
-                conv1(32, 32),
-                conv1(64, 32),
-                conv1(128, 32),
-                conv1(256, 32)
-            ])
+            tmp = nn.ModuleList([conv1(32, 32), conv1(64, 32), conv1(128, 32), conv1(256, 32)])
             self.tconv_1x1.append(tmp)
-
+        self.sconv_3x3 = nn.ModuleList()
+        for _ in range(4):
+            tmp = nn.ModuleList([conv3(32, 32), conv3(64, 32), conv3(128, 32), conv3(256, 32)])
+            self.sconv_3x3.append(tmp)
         for _ in range(4):
             tmp = nn.ModuleList([nn.Linear(3840, 512), nn.Linear(512, 512)])
             self.t_fc.append(tmp)
 
-        self.group = nn.ModuleList([
-            nn.Linear(1024, 26),
-            nn.Linear(1024, 12),
-            nn.Linear(1024, 18),
-            nn.Linear(1024, 24)
-        ])
+        self.group = nn.ModuleList(
+            [nn.Linear(1024, 26), nn.Linear(1024, 12),
+             nn.Linear(1024, 18), nn.Linear(1024, 24)])
 
         self.iablock = nn.ModuleList()
         for _ in range(4):
             tmp = nn.ModuleList([
-                Block(32, 64, reduction, scale),
+                Block(32, 32, reduction, scale),
+                Block(64, 64, reduction, scale),
                 Block(96, 128, reduction, scale),
                 Block(160, 256, reduction, scale),
-                Block(288, 512, reduction, scale),
-                Block(128, 128, reduction, scale)
+                Block(288, 128, reduction, scale)
             ])
             self.iablock.append(tmp)
 
@@ -188,22 +167,16 @@ class psnet(nn.Module):
 
     def forward(self, input):
         self.output = []
-        s_1 = self.s_conv_1(input)
-        s_1 = self.s_conv_2(s_1)
         t1 = self.t_conv[0][0](input)
         t1 = self.t_conv[0][1](t1)
         t2 = self.t_conv[1][0](input)
-        t2 = self.t_conv[1][1](t2)        
+        t2 = self.t_conv[1][1](t2)
         t3 = self.t_conv[2][0](input)
         t3 = self.t_conv[2][1](t3)
         t4 = self.t_conv[3][0](input)
         t4 = self.t_conv[3][1](t4)
-        t1 = torch.cat((t1,s_1),1)
-        t2 = torch.cat((t2,s_1),1)
-        t3 = torch.cat((t3,s_1),1)
-        t4 = torch.cat((t4,s_1),1)
 
-        block_1, s_1 = self.block([t1, t2, t3, t4], s_1, 0)
+        block_1, s_1 = self.block([t1, t2, t3, t4], input, 0)
 
         block_2, s_2 = self.block(block_1, s_1, 1)
 
@@ -234,15 +207,13 @@ class psnet(nn.Module):
         output_0, output_1, output_2, output_3 = self.output
         return output_0, output_1, output_2, output_3
 
+
 class SELayer(nn.Module):
     def __init__(self, channel, reduction=16):
         super(SELayer, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, int(channel / reduction), bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(int(channel / reduction), channel, bias=False),
-            nn.Sigmoid())
+        self.fc = nn.Sequential(nn.Linear(channel, int(channel / reduction), bias=False), nn.ReLU(inplace=True),
+                                nn.Linear(int(channel / reduction), channel, bias=False), nn.Sigmoid())
 
     def forward(self, x):
         b, c, _, _ = x.size()
