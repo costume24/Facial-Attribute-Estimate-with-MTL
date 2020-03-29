6797519439ba0d22ae845a51332f0d2e5c4e1248
costume24 <862453073@qq.com>
2020-03-29 23:10:59 +0800

    delete old

--- PS-MCNN_1.0.1/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from .resnet import *
-from .mobilenetv1 import *
-from .mobilenetv2 import *
-from .psmcnn_se_1 import *
-from .psmcnn_se_2 import *
-from .psmcnn_se_3 import *
-
